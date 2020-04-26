442317bda3460fa7d6c37bc4c95e18626f4bbfa8
Soner Boztas <37414957+SonerBoztas@users.noreply.github.com>
2020-04-26 18:29:54 +0300

    Update 2_GPIO_InputApplication_Button_LEDBLINK.c

--- 002_GPIO_InputApplication_Button_LEDBLINK/2_GPIO_InputApplication_Button_LEDBLINK.c
+++ 002_GPIO_InputApplication_Button_LEDBLINK/2_GPIO_InputApplication_Button_LEDBLINK.c
@@ -8,7 +8,7 @@ void GPIO_Config()
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
 	GPIO_InitStructure.GPIO_Mode	=	GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_Pin		= 	GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Pin	= 	GPIO_Pin_5;
 	GPIO_InitStructure.GPIO_OType	= 	GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd	= 	GPIO_PuPd_NOPULL;
 	GPIO_InitStructure.GPIO_Speed	=	GPIO_Speed_100MHz;
