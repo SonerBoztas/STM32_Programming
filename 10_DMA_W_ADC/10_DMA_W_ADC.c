2df18bb7c2f678737383231ec9d1e6046eaebac1
Soner Boztas <37414957+SonerBoztas@users.noreply.github.com>
2020-05-01 19:09:41 +0300

    Update 10_DMA_W_ADC.c

--- 10_DMA_W_ADC/10_DMA_W_ADC.c
+++ 10_DMA_W_ADC/10_DMA_W_ADC.c
@@ -4,17 +4,17 @@
 
 uint16_t adc_value = 0;
 
-GPIO_InitTypeDef		GPIO_InitStructure;
-ADC_InitTypeDef			ADC_InitStructure;
+GPIO_InitTypeDef	GPIO_InitStructure;
+ADC_InitTypeDef		ADC_InitStructure;
 ADC_CommonInitTypeDef	ADC_CommonInitStructure;
-DMA_InitTypeDef			DMA_InitStructure;
+DMA_InitTypeDef		DMA_InitStructure;
 
 void GPIO_Config()
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
 	GPIO_InitStructure.GPIO_Mode	= GPIO_Mode_OUT;
-	GPIO_InitStructure.GPIO_Pin		= GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Pin	= GPIO_Pin_5;
 	GPIO_InitStructure.GPIO_OType	= GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd	= GPIO_PuPd_NOPULL;
 	GPIO_InitStructure.GPIO_Speed	= GPIO_Speed_100MHz;
@@ -22,7 +22,7 @@ void GPIO_Config()
 	GPIO_Init(GPIOA, &GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Mode	= GPIO_Mode_AN;
-	GPIO_InitStructure.GPIO_Pin		= GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Pin	= GPIO_Pin_0;
 	GPIO_InitStructure.GPIO_OType	= GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_PuPd	= GPIO_PuPd_NOPULL;
 	GPIO_InitStructure.GPIO_Speed	= GPIO_Speed_100MHz;
@@ -34,25 +34,25 @@ void DMA_Config()
 {
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_DMA2, ENABLE);
 
-	DMA_InitStructure.DMA_Channel				= DMA_Channel_0;
+	DMA_InitStructure.DMA_Channel			= DMA_Channel_0;
 
 	DMA_InitStructure.DMA_PeripheralBaseAddr	= (uint32_t) &ADC1 -> DR;
 	DMA_InitStructure.DMA_Memory0BaseAddr		= (uint32_t) &adc_value;
 
-	DMA_InitStructure.DMA_DIR					= DMA_DIR_PeripheralToMemory;
-	DMA_InitStructure.DMA_BufferSize			= buffer_length;
+	DMA_InitStructure.DMA_DIR			= DMA_DIR_PeripheralToMemory;
+	DMA_InitStructure.DMA_BufferSize		= buffer_length;
 
-	DMA_InitStructure.DMA_PeripheralInc			= DMA_PeripheralInc_Disable;  // PINC
-	DMA_InitStructure.DMA_MemoryInc				= DMA_MemoryInc_Enable;		  // MINC
+	DMA_InitStructure.DMA_PeripheralInc		= DMA_PeripheralInc_Disable;  // PINC
+	DMA_InitStructure.DMA_MemoryInc			= DMA_MemoryInc_Enable;		  // MINC
 
 	DMA_InitStructure.DMA_PeripheralDataSize	= DMA_PeripheralDataSize_HalfWord;  // Byte: 8Bit, HalfWord: 16 Bit, Word: 32Bit
 	DMA_InitStructure.DMA_MemoryDataSize		= DMA_MemoryDataSize_HalfWord;
 
-	DMA_InitStructure.DMA_Mode					= DMA_Mode_Circular;
+	DMA_InitStructure.DMA_Mode			= DMA_Mode_Circular;
 
-	DMA_InitStructure.DMA_Priority				= DMA_Priority_High;
+	DMA_InitStructure.DMA_Priority			= DMA_Priority_High;
 
-	DMA_InitStructure.DMA_FIFOMode				= DMA_FIFOMode_Disable;
+	DMA_InitStructure.DMA_FIFOMode			= DMA_FIFOMode_Disable;
 
 	DMA_Init(DMA2_Stream0, &DMA_InitStructure);
 
@@ -63,14 +63,14 @@ void ADC_Config()
 {
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 
-	ADC_CommonInitStructure.ADC_Mode			= ADC_Mode_Independent;
+	ADC_CommonInitStructure.ADC_Mode		= ADC_Mode_Independent;
 	ADC_CommonInitStructure.ADC_Prescaler		= ADC_Prescaler_Div4;
 	ADC_CommonInitStructure.ADC_DMAAccessMode	= ADC_DMAAccessMode_Disabled;
 
 	ADC_CommonInit(&ADC_CommonInitStructure);
 
-	ADC_InitStructure.ADC_Resolution			= ADC_Resolution_12b;
-	ADC_InitStructure.ADC_DataAlign				= ADC_DataAlign_Right;
+	ADC_InitStructure.ADC_Resolution		= ADC_Resolution_12b;
+	ADC_InitStructure.ADC_DataAlign			= ADC_DataAlign_Right;
 	ADC_InitStructure.ADC_ContinuousConvMode	= ENABLE;
 
 	ADC_Init(ADC1, &ADC_InitStructure);
